87a3f4cccd53ec2b7cadb15f8a5024af282c8d38
BLz13 <bruno.lazo@outlook.com>
2025-09-24 19:25:54 -0300

    Add a chevron logo to the location display

--- src/components/location/location.jsx
+++ src/components/location/location.jsx
@@ -2,13 +2,20 @@ import './location.scss';
 
 import { useEffect, useState } from 'react';
 
+import Chevron from '../../assets/svg/chevron-down.svg?react';
+
 export default function Location({ location, toggleLocationFilter, locationFilterState }) {
 
     const [ubicacion, setUbicacion] = useState(location);
+    const [arrowClass, setArrowClass] = useState('arrow-down');
     
     useEffect(() => {
         setUbicacion(location);
     }, [location]);
+
+    useEffect(() => {
+        setArrowClass(`arrow-down${locationFilterState ? ' open' : ''}`);
+    }, [locationFilterState]);
 
     const handleClick = () => {
         toggleLocationFilter(!locationFilterState);
@@ -17,17 +24,20 @@ export default function Location({ location, toggleLocationFilter, locationFilte
     return (
         <div className="location" onClick={handleClick} onTouchEnd={handleClick}>
             {!ubicacion ? (
-                <h2 className='main-location'>Argentina</h2>
+                <div>
+                    <h2 className='main-location'>Argentina</h2>
+                </div>
             ) : (
-                <>
+                <div>
                     <h2 className='main-location'>
                         {`${ubicacion.departamento.nombre}`}
                     </h2>
                     <h3 className='secondary-location'>
                         {`${ubicacion.municipio.nombre}, ${ubicacion.provincia.nombre}`}
                     </h3>
-                </>
+                </div>
             )}
+            <Chevron className={arrowClass} />
         </div>
     );
 }
